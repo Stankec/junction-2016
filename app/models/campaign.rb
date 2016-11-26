cdeba135e64ae4e68d37fae6db7d5d8346996a7d
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-11-26 06:31:27 +0100

    Annotate

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: campaigns
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  site_data  :json
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Campaign < ApplicationRecord
 end
--- test/models/campaign_test.rb
+++ test/models/campaign_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: campaigns
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  site_data  :json
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CampaignTest < ActiveSupport::TestCase
