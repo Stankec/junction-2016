fcedac1a91327625833f5905013618ba99f75387
lstrz <->
2016-11-26 22:27:40 +0100

    Made seed db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+## USER 1
+# Clothes
+Mapping.create(twitter_user_id: '415859364', tag_list: 'Nike,wear,sport,active,sneakers,shoes')
+Mapping.create(twitter_user_id: '183398746', tag_list: 'Converse,unisex,cool,wear,sneakers,shoes')
+Mapping.create(twitter_user_id: '50883209', tag_list: 'Puma,wear,active,sport')
+Mapping.create(twitter_user_id: '20763485', tag_list: 'Speedo,active,swim,sport,pool')
+Mapping.create(twitter_user_id: '85772748', tag_list: 'Dolce & Gabbana,wear,fashion,fancy')
+Mapping.create(twitter_user_id: '16913418', tag_list: 'Gucci,wear,fashion,fancy')
+Mapping.create(twitter_user_id: '300114634', tag_list: 'Adidas,wear,sport,active')
+Mapping.create(twitter_user_id: '18481641', tag_list: 'LACOSTE,fancy,wear,fashion,comfortable')
+
+# Music
+Mapping.create(twitter_user_id: '29374598', tag_list: 'Sum 41,music,punk,rock,punk rock')
+Mapping.create(twitter_user_id: '89366493', tag_list: 'Avenged Sevenfold,music,A7X,metal,heavy metal,nu metal,thrash metal')
+Mapping.create(twitter_user_id: '80530889', tag_list: 'Apocalyptica,music,cello')
+Mapping.create(twitter_user_id: '100220864', tag_list: 'Bruno Mars,music,pop,funk,soul')
+
+# IT
+Mapping.create(twitter_user_id: '457033547', tag_list: 'DigitalOcean,IT,hosting,virtual machine,vm,cloud')
+Mapping.create(twitter_user_id: '13334762', tag_list: 'GitHub,IT,git,repository')
+Mapping.create(twitter_user_id: '2459371', tag_list: 'Skype,IT,voip,messaging,calling')
+Mapping.create(twitter_user_id: '15531582', tag_list: 'Unity,IT,engine,games,vr')
+Mapping.create(twitter_user_id: '74286565', tag_list: 'Microsoft,IT,vr,ar,hardware,os,Windows,software,cloud')
+Mapping.create(twitter_user_id: '380749300', tag_list: 'Apple,IT,hardware,iPhone,Software,Mac,MacBook,iTunes')
+
+## USER 2
+# Clothes
+# Puma, Speedo, Gucci, Adidas
+
+# Music
+# Sum 41, Bruno Mars
+
+# IT
+# Unity, DigitalOcean
