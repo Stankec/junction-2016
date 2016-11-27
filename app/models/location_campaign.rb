46dfa64d2fd1f6c04354b27005d574e5939e0733
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-11-27 01:22:27 +0100

    Annotate

--- app/models/location_campaign.rb
+++ app/models/location_campaign.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: location_campaigns
+#
+#  id          :integer          not null, primary key
+#  location_id :integer
+#  campaign_id :integer
+#  points      :integer          default(0), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class LocationCampaign < ApplicationRecord
   belongs_to :location
   belongs_to :campaign
--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: mappings
+#
+#  id              :integer          not null, primary key
+#  twitter_user_id :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  display_name    :string           default(""), not null
+#
+
 class Mapping < ApplicationRecord
   acts_as_taggable
 
